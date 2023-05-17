6289b18eb339611e8dcde664071f4153972ebc33
Jorge Harbes <jharbes@hotmail.com>
2023-05-17 05:21:17 -0300

    aula 05d

--- 05d-Aula - 15MAI23 - Teclado - LCD - Servo/Exemplo_9 Servo_PIC18F46K22/servo.h
+++ 05d-Aula - 15MAI23 - Teclado - LCD - Servo/Exemplo_9 Servo_PIC18F46K22/servo.h
@@ -1,15 +1,8 @@
 #include <18F46K22.h>
 #device ADC=10
 
-#FUSES NOWDT                    //No Watch Dog Timer
-#FUSES WDT128                   //Watch Dog Timer uses 1:128 Postscale
-#FUSES NOBROWNOUT               //No brownout reset
-#FUSES NOLVP                    //No low voltage prgming, B3(PIC16) or B5(PIC18) used for I/O
-#FUSES NOXINST                  //Extended set extension and Indexed Addressing mode disabled (Legacy mode)
-
-#use delay(internal=8MHz,restart_wdt)
+#use delay(internal=8MHz)
 #use rs232(baud=9600,parity=N,xmit=PIN_C6,rcv=PIN_C7,bits=8,stream=PORT1)
-
 
 // acesso aos pinos:
 #define LCD_ENABLE_PIN  PIN_E1
--- 05d-Aula - 15MAI23 - Teclado - LCD - Servo/Teste_LED_Wizard/main.h
+++
@@ -1,8 +0,0 @@
-#include <18F4520.h>
-
-#use delay(internal=1MHz)
-
-#define LED 		PIN_A0
-#define DELAY 	100
-
-
